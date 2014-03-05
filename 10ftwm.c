e04b93a5d4b08c8bc947413e908692dc5810c4f0
Berulacks <beru@eml.cc>
2014-03-04 21:39:21 -0500

    Added some more event handling code. The WM is now actually a WM.

--- 10ftwm.c
+++ 10ftwm.c
@@ -10,15 +10,23 @@ int main (int argc, char **argv)
 	uint32_t values[2];
 	uint32_t mask = 0;
 
+	//Our connection to the xServer
 	xcb_connection_t *connection;
+	//Our primary screen
 	xcb_screen_t *screen;
+	//Our main graphics context
+	//So... the background?
 	xcb_drawable_t mainGC;
 
+	// #justeventthings
 	xcb_generic_event_t *ev;
 	//xcb_get_geometry_reply_t *geom;
 
 	xcb_gcontext_t    black;
-	
+	xcb_void_cookie_t cookie;
+
+	xcb_generic_error_t *error;
+
 	printf("Starting up!\n");
 	
 	//Don't have a display name or number,
@@ -43,30 +51,48 @@ int main (int argc, char **argv)
 
 	mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
 	values[0] = screen->white_pixel;
-	values[1] = XCB_EVENT_MASK_EXPOSURE;
+	values[1] = XCB_EVENT_MASK_BUTTON_PRESS;
 
 	xcb_create_window( connection, XCB_COPY_FROM_PARENT, window1, mainGC, 0,0 , 150,150 , 14, XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual, mask, values);
 	xcb_map_window( connection, window1 );
 
-	black = xcb_generate_id (connection);
-	xcb_create_gc (connection, black, mainGC, XCB_GC_BACKGROUND, &screen->white_pixel);
+	//black = xcb_generate_id (connection);
+	//xcb_create_gc (connection, black, mainGC, XCB_GC_BACKGROUND, &screen->white_pixel);
 
 
 
 
 
 
-	xcb_grab_key(connection, 1, mainGC, XCB_MOD_MASK_2, XCB_NO_SYMBOL,
+	xcb_grab_key(connection, 1, mainGC, XCB_MOD_MASK_1, XCB_NO_SYMBOL,
 		 XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
 
 	xcb_grab_button(connection, 0, mainGC, XCB_EVENT_MASK_BUTTON_PRESS |
 		XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
 		XCB_GRAB_MODE_ASYNC, mainGC, XCB_NONE, 1, XCB_MOD_MASK_1);
 
+
+
 	xcb_grab_button(connection, 0, mainGC, XCB_EVENT_MASK_BUTTON_PRESS |
 		XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC,
 		XCB_GRAB_MODE_ASYNC, mainGC, XCB_NONE, 3, XCB_MOD_MASK_1);
-	xcb_flush(connection);
+
+	/* Subscribe to events. */
+	mask = XCB_CW_EVENT_MASK;
+
+	values[0] = XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
+		  | XCB_EVENT_MASK_STRUCTURE_NOTIFY
+		  | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY;
+
+	cookie = xcb_change_window_attributes_checked(connection, mainGC, mask, values);
+	error = xcb_request_check(connection, cookie);
+
+	if(error != NULL)
+	{
+		printf("Are you, like, trying to run TWO window managers or something?");
+		exit(1);
+
+	}
 
 
 	xcb_flush(connection);
@@ -80,21 +106,32 @@ int main (int argc, char **argv)
 
 		case XCB_BUTTON_PRESS:
 		{
-			printf("Dat button press");
+			printf("Dat button press\n");
 		}
 		break;
 
 		case XCB_MOTION_NOTIFY:
 		{
-			printf("Dat motion");
+			printf("Dat motion\n");
 		}
 		break;
 		case XCB_MAP_REQUEST:
 		{
-		    xcb_map_request_event_t *e;
 
-		    e = (xcb_map_request_event_t *) ev;
-		    addWindow(e->window);
+			printf("OH MAN I MADE A FRIEND!\n");
+			xcb_map_request_event_t *e;
+
+			e = (xcb_map_request_event_t *) ev;
+			//addWindow(e->window);
+			xcb_window_t win = e->window;
+			mask = XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y;
+			values[0] = 10;
+			values[1] = 10;
+
+			xcb_configure_window( connection, win, mask, values);
+			xcb_map_window(connection, win);
+
+
 		}
 		break;
 
@@ -108,7 +145,12 @@ int main (int argc, char **argv)
 		}
 	}
 
-
 	return 0;
+
+}
+
+void addWindow( xcb_window_t e )
+{
+	
 
 }
